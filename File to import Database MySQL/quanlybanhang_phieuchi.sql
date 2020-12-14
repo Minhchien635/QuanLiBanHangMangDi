3fefd190ed25be81e5aa22979a55e34c49b7dc36
Minhchien635 <minhchien0407.vk.1718@gmail.com>
2020-12-14 10:31:48 +0700

    cập nhật Thêm phiếu chi

--- File to import Database MySQL/quanlybanhang_phieuchi.sql
+++ File to import Database MySQL/quanlybanhang_phieuchi.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `phieuchi`;
 CREATE TABLE `phieuchi` (
   `ma` varchar(15) NOT NULL,
   `manhanvien` int DEFAULT NULL,
-  `ngay` date DEFAULT NULL,
+  `ngay` datetime DEFAULT NULL,
   `tonggia` int DEFAULT NULL,
   PRIMARY KEY (`ma`),
   KEY `FK_NhanVien01` (`manhanvien`),
@@ -39,7 +39,7 @@ CREATE TABLE `phieuchi` (
 
 LOCK TABLES `phieuchi` WRITE;
 /*!40000 ALTER TABLE `phieuchi` DISABLE KEYS */;
-INSERT INTO `phieuchi` VALUES ('00000000',1,'2020-12-05',2070000),('00000001',1,'2020-12-11',600000);
+INSERT INTO `phieuchi` VALUES ('00000000',1,'2020-12-14 10:16:38',360000);
 /*!40000 ALTER TABLE `phieuchi` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-11 17:58:10
+-- Dump completed on 2020-12-14 10:19:34
